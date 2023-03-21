512770cc61c2e0ba21c22f14989a51c26c61922f
MLibralato91 <michelelibralato911@gmail.com>
2023-03-21 15:39:36 +0100

    add thw while loop for the rnd number

--- js/script.js
+++ js/script.js
@@ -7,10 +7,31 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
  */
 
 
-const numberPc =[];
+const numberPc = [];
+let numbers = document.getElementById('numbers');
 
-//while(numberPc.length < 5){
-    // genero numeri random tra 1 e 100
-    // verifico con includes e poi pusho
-//}
+function getRndNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+
+setInterval(() => {
+    while (numberPc.length < 5) {
+        // genero numeri random tra 1 e 100
+        num = getRndNumber(1, 100);
+        if (!numberPc.includes(num))
+            numberPc.push(num);
+        
+        
+        // verifico con includes e poi pusho
+    
+    }
+    numbers.innerText = numberPc;
+    
+
+}, 5000);
+
+
+
+
+console.log(numberPc);
